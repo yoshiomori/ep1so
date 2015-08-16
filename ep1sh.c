9f6e4e4c9d3fe8cb01f6f4bfa22d50d2626736b9
yoshiomori <yoshiomori90@gmail.com>
2015-08-16 12:08:03 -0300

    Implementação
    Código inicial

---
+++ ep1sh.c
@@ -0,0 +1,13 @@
+int main(){
+while(1){
+/* Tratar Mensagens do usuário enviadas pelo teclado */
+
+/* Enviar comandos para SO */
+
+/* Receber resposta do SO */
+
+/* Enviar mensagens para o terminal */
+
+}
+return 0;
+}
