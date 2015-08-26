845acdf4d93b6c961a2885c2781b55a6ce243142
Yoshio Mori <yoshio.mori@hotmail.com>
2015-08-26 00:24:07 -0300

    Implementação
    Criando processos com o fork

--- ep1sh.c
+++ ep1sh.c
@@ -1,13 +1,16 @@
 #include "mallocSafe.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define NUM_MAX_INICIAL_CARAC_A_LER 100
 #define PATH_MAX 4096
 
 int main(){
 	char *pcwd;
+	pid_t childPid;
     while(1){
         /* Imprimir o diretório */
 	pcwd = getcwd(NULL, 0);
@@ -20,6 +23,19 @@ int main(){
 	fgets(entrada, numMaxCaracALer, stdin);        
         
         /* Enviar comandos para SO (fork) */
+	switch (childPid = fork()){
+	case -1:
+	  perror("fork failed");
+	  exit(1);
+	case 0:
+	  /* Processo do filho */
+	  printf("execve para o comand %s\n", entrada);
+	  break;
+	default:
+	  /* Processo do pai */
+	  wait(NULL);
+	  break;
+	}
 
         /* Enviar mensagens para o terminal ? */
 
