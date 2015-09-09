75b6735a9311062a534a0eb0eedf7884c1babab3
Yoshio Mori <yoshio.mori@hotmail.com>
2015-09-09 11:01:14 -0300

    Implementação
    Abrindo o arquivo

--- ep1.c
+++ ep1.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+  FILE *pfile;
+  int modo;
   if(argc < 4){
     printf("Falta argumentos\n");
     exit(0);
   }
+  pfile = fopen(argv[2], "r");
+  if(!pfile){
+    perror(argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  
+  fclose(pfile);
   return 0;
 }
