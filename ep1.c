43114285d66b2123a607bc4216d014826a0cea01
Marcelo Yukio Iyama Silvarolla <marcelo.silvarolla@gmail.com>
2015-09-10 19:18:44 -0300

    leitura de traces implementada

--- ep1.c
+++ ep1.c
@@ -1,19 +1,73 @@
+#define _GNU_SOURCE
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+
+#include "mallocSafe.h"
+
+#define NUM_MAX_PROC (INT_MAX - 10)
+#define TAM_MAX_NOME_PROC 1000
+
+void ***proc;
 
 int main(int argc, char **argv){
-  FILE *pfile;
-  int modo;
+  FILE *arqTraces;
+  /* 1. Verificar numero de argumentos */
   if(argc < 4){
     printf("Falta argumentos\n");
     exit(0);
   }
-  pfile = fopen(argv[2], "r");
-  if(!pfile){
+
+  /* 2. Abrir arquivo de traces */
+  arqTraces = fopen(argv[2], "r");
+  if(!arqTraces){
     perror(argv[1]);
     exit(EXIT_FAILURE);
   }
   
-  fclose(pfile);
+  /* 3. Armazenar traces na tabela proc */
+  proc = mallocSafe(NUM_MAX_PROC);
+  int j;
+  for(j = 0; j < NUM_MAX_PROC; j++) {
+    proc[j] = mallocSafe(9*sizeof(void**));
+    proc[j][0] = (void*)mallocSafe(sizeof(double));
+    proc[j][1] = (void*)mallocSafe(TAM_MAX_NOME_PROC*sizeof(char));
+    proc[j][2] = (void*)mallocSafe(sizeof(double));
+    proc[j][3] = (void*)mallocSafe(sizeof(double));
+    proc[j][4] = (void*)mallocSafe(sizeof(int));
+    if(fscanf(arqTraces, "%lf %s %lf %lf %d\n", (double*)proc[j][0],
+            (char*)proc[j][1], (double*)proc[j][2],
+            (double*)proc[j][3], (int*)proc[j][4]) != 5) {
+      /* esta linha eh a primeira linha vazia do arquivo
+       * entao liberamos a memoria que alocamos desnecessariamente
+       */
+      int l;
+      for(l = 0; l < 5; l++) {
+        free(proc[j][l]);
+      }
+      free(proc[j]);
+      break;
+    }
+  }
+
+  int numProc = j;
+  /* TESTE COMECA */
+  for(j = 0; j < numProc; j++) {
+    printf("%lf %s %lf %lf %d\n", *(double*)proc[j][0], 
+            (char*)proc[j][1], *(double*)proc[j][2],
+            *(double*)proc[j][3], *(int*)proc[j][4]);
+  }
+
+
+  /* TESTE TERMINA */
+
+
+  
+  
+
+
+  fclose(arqTraces);
   return 0;
 }
--- makefile
+++ makefile
@@ -8,6 +8,9 @@ LDFLAGS = -g
 
 LDLIBS = 
 
+.PHONY: all
+all: ep1sh ep1
+
 ep1sh: ep1sh.o mallocSafe.o leituraArq.o
 #	$(CC) $(LDFLAGS) ep1sh.o mallocSafe.o $(LDLIBS)
 #	(regra implicita do makefile)
@@ -15,16 +18,15 @@ ep1sh: ep1sh.o mallocSafe.o leituraArq.o
 ep1sh.o: ep1sh.c mallocSafe.h leituraArq.h
 #	$(CC) -c $(CFLAGS) ep1sh.c
 
+ep1: ep1.o mallocSafe.o
 
+ep1.o: ep1.c mallocSafe.h
 
 .PHONY: clean
 
 clean:
 	rm -f *.o 
 
-.PHONY: all
-
-all: clean main
 
 
 
